197b243ef3ff00720490a3cf9e67669d56072208
ben.sless <ben.sless@appsflyer.com>
2020-02-17 18:02:21 +0200

    Bump core.async dependency version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "0.5.527"]]
+                 [org.clojure/core.async "0.7.559"]]
 
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
