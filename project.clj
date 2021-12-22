2d11df7ccb0bd422f742cad618b4487a7745e9fe
Ben Sless <ben.sless@gmail.com>
2021-12-22 12:40:11 +0200

    Bump deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/bsless/more.async"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "1.0.567"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/core.async "1.5.648"]]
 
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_user
